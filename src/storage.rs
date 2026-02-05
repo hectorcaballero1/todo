a36bf341fee8de3a385c855f3eebd20f6f5f0f72
hector <hectorcaballero1205@gmail.com>
2026-02-04 20:07:22 -0500

    feat: add list_all function to storage

--- src/storage.rs
+++ src/storage.rs
@@ -1,6 +1,6 @@
-use std::{fs, io, path::PathBuf};
-use serde_json;
 use crate::todo::Todo;
+use serde_json;
+use std::{fs, io, path::PathBuf};
 
 fn get_data_dir() -> PathBuf {
     PathBuf::from("./data")
@@ -8,14 +8,12 @@ fn get_data_dir() -> PathBuf {
 
 pub fn save(list: &Todo) -> io::Result<()> {
     let dir = get_data_dir();
-    fs::create_dir_all(&dir)?; 
+    fs::create_dir_all(&dir)?;
     let json = serde_json::to_string_pretty(list)?;
     let file_path = dir.join(format!("{}.json", list.name()));
     fs::write(file_path, json)?;
     Ok(())
 }
-
-
 pub fn load(name: &str) -> io::Result<Todo> {
     let file_path = get_data_dir().join(format!("{}.json", name));
     let content = fs::read_to_string(file_path)?;
@@ -26,4 +24,18 @@ pub fn load(name: &str) -> io::Result<Todo> {
 pub fn exists(name: &str) -> bool {
     let file_path = get_data_dir().join(format!("{}.json", name));
     file_path.exists()
+}
+
+pub fn list_all() -> io::Result<Vec<String>> {
+    let dir = get_data_dir();
+    let mut names: Vec<String> = Vec::new();
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        if let Some(name) = entry.file_name().to_str() {
+            if let Some(name_without_suffix) = name.strip_suffix(".json") {
+                names.push(name_without_suffix.to_string());
+            }
+        }
+    }
+    Ok(names)
 }
